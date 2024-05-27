2fb8615db377817aa3b5b6ef249181b6fe85b636
maricardosoo <contato@maricardoso.com>
2024-05-27 13:48:26 -0700

    add http

--- src/server.js
+++ src/server.js
@@ -2,15 +2,20 @@ import express from 'express'
 import userRouter from './routes/user.js'
 import concertRouter from './routes/concert.js'
 import listRouter from "./routes/list.js"
+import http from 'http'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(concertRouter);
 app.use(listRouter);
 
-app.listen(8080, () => {
+const httpServer = http.createServer(app);
+
+httpServer.listen(8080, () => {
     console.log('Server is running on port 8080')
 });
 
